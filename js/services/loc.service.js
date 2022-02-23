2852a476578117cb4d26d1f863570d86826be595
Yonatan Erez <jontaner12@gmail.com>
2022-02-23 17:12:40 +0200

    stoage file added

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,10 +1,14 @@
+import { storageService } from './storage.service.js';
+
+const STORAGE_KEY = 'locationsDB'
+
 export const locService = {
     getLocs
 }
 
 
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
+    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
     { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
 ]
 
@@ -15,5 +19,3 @@ function getLocs() {
         }, 2000)
     });
 }
-
-
